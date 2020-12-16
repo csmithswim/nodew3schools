5ff788c7f80276438d1844304b0272f94eac1b3d
Cullen S <cullensmi@gmail.com>
2020-12-16 17:00:35 -0500

    finished walkthrough

--- nodew3schools/myfirst.js
+++ nodew3schools/myfirst.js
@@ -1,18 +1,96 @@
-//http module to make server
-var http = require('http');
-//custom made module
-var dt = require('./myfirstmodule');
-//URL module
+var http =  require('http');
 var url = require('url');
-//file system module
 var fs = require('fs');
 
-//creates a sever object:
-//function fires whenever someone goes to that port on their browser
-http.createServer(function (req, res) {
-    //Second argument is an object that is the response headers and the first argument is the status code
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);//end response
-}).listen(8080);//server object listens on this port
+http.createServer(function(req, res){
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //http module to make server
+// var http = require('http');
+// //custom made module
+// var dt = require('./myfirstmodule');
+// //URL module
+// var url = require('url');
+// //file system module
+// var fs = require('fs');
+
+
+// //fs can create new files with the appendFile() method.
+// fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+//   }); 
+
+// //fs.open() method takes a "flag" as a second argument, if the flag is "w" for "writing", the specified file is opened for writing. If the file does not exist, an empty file is created:
+// fs.open('mynewfile2.txt', 'w', function (err, file) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+// //fs.writeFile() method replaces the specified file and content if it exists. If the file does not exist, a new file, containing the specified content, will be created:
+// fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+//   }); 
+
+// //fs.unlink() method deletes a file with fs.
+// var fs = require('fs');
+
+// fs.unlink('mynewfile2.txt', function (err) {
+//   if (err) throw err;
+//   console.log('File deleted!');
+// }); 
+
+// //To rename a file in fs use fs.rename()
+// var fs = require('fs');
+
+// fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+//   if (err) throw err;
+//   console.log('File Renamed!');
+// }); 
+
+// //creates a sever object:
+// //function fires whenever someone goes to that port on their browser
+// http.createServer(function (req, res) {
+//     fs.readFile('demofile1.html', function(err, data) {
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);//server object listens on this port.
